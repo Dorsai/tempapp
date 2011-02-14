ca72e429a377650836a53e2b2c76f0dbe2963388
Skip Levens <skip@giantravens.com>
2011-02-14 00:01:05 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tempapp::Application.config.secret_token = '5fd29cb8729ca16fbcd201706ac328c75107efa588d6ec487cd4abd5e60d36347d4d3c0e748811f7e11f2263435227c4d10dc612e0f075114541c67918aad671'
